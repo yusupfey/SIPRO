b3aebf23d276b64dc021f959e9085765e7297a6b
yusupfey <34081645+yusupfey@users.noreply.github.com>
2020-03-06 10:42:01 +0700

    menambahkan landing-page

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -19,6 +19,91 @@
             min-height: 140px;
             border: 1px solid red;
         }
+
+        .jumbotron {
+            background-image: url("<?= base_url(); ?>assets/pic/bg.jpg");
+            /* background-size: cover; */
+            /* background-attachment: fixed; */
+            background-position: 0 -100px;
+            color: #eaeaea;
+            box-shadow: 1px 1px 10px rgb(0, 0, 0, 0, 5);
+            position: relative;
+        }
+
+        .jumbotron h1 {
+            text-shadow: 1px 1px 10px rgb(0, 0, 0, 0, 5);
+        }
+
+        .form-src {
+            margin-top: 200px;
+        }
+
+        .nav-cust {
+            min-height: 90px;
+            font-size: 20px;
+        }
+
+        .jumbotron::after {
+            content: '';
+            display: block;
+            width: 100%;
+            height: 100%;
+            bottom: 0px;
+            position: absolute;
+            background-image: linear-gradient(to top, rgba(20, 20, 20, 2), rgba(0, 0, 0, 0));
+            margin-left: -18px;
+            margin-right: 0px;
+        }
+
+        .display-4,
+        .lead,
+        .form-src {
+            z-index: 1;
+            position: relative;
+        }
+
+        .cari-panel {
+            box-shadow: 0 3px 3px rgba(0, 0, 0.5);
+            padding: 30px;
+            border-radius: 12px;
+            margin-top: -300px;
+            position: relative;
+        }
+
+        @media (min-width: 992px) {
+            .nav-link {
+                margin-left: 20px;
+            }
+
+            .nav-link:hover {
+                border-bottom: 4px solid blue;
+            }
+
+            .cari-panel {
+                background-color: white;
+                box-shadow: 0 3px 3px rgba(0, 0, 0.5);
+                padding: 30px;
+                border-radius: 12px;
+                margin-top: -90px;
+                position: relative;
+            }
+
+            .jumbotron::after {
+                content: '';
+                display: block;
+                width: 100%;
+                height: 100%;
+                bottom: 0px;
+                position: absolute;
+                background-image: linear-gradient(to top, rgba(20, 20, 20, 2), rgba(0, 0, 0, 0));
+                margin-left: -32px;
+                margin-right: 0px;
+            }
+
+            .active {
+                border-bottom: 4px solid blue;
+            }
+        }
     </style>
 
 </head>
--- application/views/template/nav-front/header.php
+++ application/views/template/nav-front/header.php
@@ -1,46 +1,95 @@
-<nav class="navbar navbar-expand-lg navbar-light sticky-top bg-light">
+<nav class="navbar navbar-expand-lg navbar-light sticky-top bg-light nav-cust">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">SIPRO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Features</a>
-                <a class="nav-item nav-link" href="#">Pricing</a>
-                <a class="nav-item nav-link disabled" href="#">Disabled</a>
+
             </div>
             <div class="navbar-nav ml-auto">
-                <a class="nav-item nav-link disabled" href="#">Login</a>
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">About</a>
+                <a class="nav-item nav-link" href="#">Kategori</a>
+                <a class="nav-item nav-link" href="#">Disabled</a>
+                <a class="nav-item nav-link" href="#">Login</a>
             </div>
         </div>
     </div>
 </nav>
-<div class="jumbotron jumbotron-pluid d-md-block d-none">
+<div class="container-pluid jumbotron">
     <h1 class="display-4">SIPRO</h1>
     <p class="lead">Membantu Anda Menemukan Rumah impian anda.</p><br>
-    <form action="" class="">
+    <form action="" class="form-src">
         <div class="row text-center">
-            <div class="col-md-8 offset-md-4" style="">
-                <div class="input-group input-group-sm mb-3">
+            <!-- <div class="col-md-6 offset-xs-6 mx-auto" style="">
+                <div class="input-group input-group-md">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroup-sizing-sm">prov</span>
-                        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
-                        <span class="input-group-text" id="inputGroup-sizing-sm">kab</span>
-                        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
-
-                        <span class="input-group-text" id="inputGroup-sizing-sm">kota</span>
-                        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
-                        <button type="button" class="btn btn-primary btn-sm">Search</button>
+                        <span class="input-group-text" id="inputGroup-sizing-md">prov</span>
+                        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-md">
+                    </div>
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-md">Kota</span>
+                        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-md">
+                    </div>
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-md">kota</span>
+                        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-md">
+                    </div>
+                    <div class="input-group-prepend">
+                        <button type="button" class="btn btn-primary btn-md m-block">Search</button>
                     </div>
                 </div>
+            </div> -->
+            <div class="col-sm-12 p-5 d-md-block d-none">
+                <img src="assets/pic/ucup.png" class="img-circle" height="200" width="200" alt="">
+                <center>
+                    <p class="line"></p>
+                </center>
+                <h1>Sistem Inforamsi Property</h1>
+                <h3>Informatika Komputer</h3>
             </div>
         </div>
-
     </form>
 </div>
 <div class="container">
+    <div class="row justify-content-center cari-panel">
+        <div class="col-lg-10">
+            <form action="">
+                <div class="row">
+                    <div class=" col-md-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-md">kota</span>
+                            <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-md">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-md">kota</span>
+                            <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-md">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-md">kota</span>
+                            <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-md">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <hr>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-block btn-success">Cari</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
     <h4>CATALOG</h4>
     <div class="row text-center">
         <div class="col-md-4 katalog">
